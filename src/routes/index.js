f23405183a44a2eb0cba46915d341d5d7c11ec92
Larry Wen <s1834299@staff.ad.griffith.edu.au>
2022-08-21 16:48:33 +1000

    add redirect for index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "LTetris Server" });
+  //  res.render("index", { title: "LTetris Server" });
+  res.redirect("/docs");
 });
 
 module.exports = router;
